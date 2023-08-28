05345f4461118983e111dd483d17576f524f4d8e
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-08-28 14:57:11 +0400

    .

--- src/main/java/com/intelliacademy/lesson/cp4/ImageProcessor.java
+++ src/main/java/com/intelliacademy/lesson/cp4/ImageProcessor.java
@@ -9,6 +9,8 @@ public class ImageProcessor {
     private String SOURCE_FILE;
     private String DESTINATION_FILE;
 
+    private BufferedImage outputImage;
+
     public ImageProcessor(String source,String output){
         this.SOURCE_FILE = source;
         this.DESTINATION_FILE = output;
@@ -18,22 +20,22 @@ public class ImageProcessor {
         BufferedImage bufferedImage = null;
         try {
             bufferedImage = ImageIO.read(new File(SOURCE_FILE));
-            var outputImage = generateOutputImage(bufferedImage);
-            this.proceedSingleThread(bufferedImage,outputImage);
-            ImageIO.write(outputImage,"jpg",new File(DESTINATION_FILE));
+            this.outputImage = generateOutputImage(bufferedImage);
+            this.proceedSingleThread(bufferedImage);
+            ImageIO.write(this.outputImage,"jpg",new File(DESTINATION_FILE));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void proceedSingleThread(BufferedImage input,BufferedImage output){
-        this.recolorImage(input,output,0,0,input.getWidth(),input.getHeight());
+    private void proceedSingleThread(BufferedImage input){
+        this.recolorImage(input,this.outputImage,0,0,input.getWidth(),input.getHeight());
     }
 
-    private void recolorImage(BufferedImage input,BufferedImage output,int leftCorner,int rightCorner,
+    private void recolorImage(BufferedImage input,BufferedImage output,int leftCorner,int topCorner,
                               int width,int height){
         for (int x = leftCorner;x < leftCorner + width && x < input.getWidth();x++){
-            for (int y = rightCorner; y < rightCorner + height && y < input.getHeight(); y++){
+            for (int y = topCorner; y < topCorner + height && y < input.getHeight(); y++){
                 this.recolorPixel(input,output,x,y);
             }
         }
@@ -47,7 +49,7 @@ public class ImageProcessor {
         int newBlue;
 
         if (rgb.isShadeOfGrey()){
-            newRed = Math.min(255,rgb.getRed());
+            newRed = Math.min(255,rgb.getRed() + 10);
             newGreen = Math.max(0,rgb.getGreen() - 80);
             newBlue = Math.max(0,rgb.getBlue() - 20);
         }else {
@@ -61,11 +63,11 @@ public class ImageProcessor {
                 .green(newGreen)
                 .blue(newBlue)
                 .build();
-        this.setRGB(outPut,x,y,newRgb.getRgb());
+        this.setRGB(x,y,newRgb.getRgb());
     }
 
-    private void setRGB(BufferedImage output,int x,int y,int rgb){
-        output.getRaster().setDataElements(x,y,output.getColorModel().getDataElements(rgb,null));
+    private void setRGB(int x,int y,int rgb){
+        this.outputImage.getRaster().setDataElements(x,y,this.outputImage.getColorModel().getDataElements(rgb,null));
     }
 
     private BufferedImage generateOutputImage(BufferedImage sourceImage){
--- src/main/java/com/intelliacademy/lesson/cp4/PerformanceOptimizationApplication.java
+++ src/main/java/com/intelliacademy/lesson/cp4/PerformanceOptimizationApplication.java
@@ -1,11 +1,11 @@
 package com.intelliacademy.lesson.cp4;
 
-public class PerformanceOptimization {
+public class PerformanceOptimizationApplication {
 
     public static void main(String[] args) {
         ImageProcessor imageProcessor  = new ImageProcessor(
-                "src/main/resources/input/flw2.jpg",
-                "src/main/resources/output/flw2.jpg"
+                "src/main/resources/input/flw3.jpg",
+                "src/main/resources/output/flw3.jpg"
         );
         imageProcessor.proceed();
     }
