7906531a031a76cdd00076cf44e6fd70730830da
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-10 19:51:13 +0400

    SequentialProcess.java

--- src/main/java/com/intelliacademy/lesson/cp2/impl/StartingThreadWithRunnable.java
+++ src/main/java/com/intelliacademy/lesson/cp2/impl/StartingThreadWithRunnable.java
@@ -3,6 +3,7 @@ package com.intelliacademy.lesson.cp2.impl;
 
 import com.intelliacademy.lesson.env.ProcessManager;
 
+import java.util.stream.IntStream;
 
 
 public class StartingThreadWithRunnable {
@@ -11,8 +12,22 @@ public class StartingThreadWithRunnable {
         //Time-slicing is a method to share CPU time between multiple threads in single processor.
         Thread thread1 = new Thread(new AnyManager("Task 1"));
         Thread thread2 = new Thread(new AnyManager("Task 2"));
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i< 100; i++){
+                    System.out.println("Task 3 proceed on " + i + " thread " + Thread.currentThread().getName());
+                }
+            }
+        });
+        Thread thread4 = new Thread(() -> IntStream.rangeClosed(0, 100).forEach(i -> {
+            System.out.println("Task 4 proceed on " + i + " thread " + Thread.currentThread().getName());
+        }));
+
         thread1.start();
         thread2.start();
+        thread3.start();
+        thread4.start();
     }
 
 }
